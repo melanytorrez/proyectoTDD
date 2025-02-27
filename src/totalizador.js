54bf21d16a428084674a1f57f4c9ea0e0b373155
Jorge Vargas <jorge.vargas.p@ucb.edu.bo>
2025-02-27 10:05:58 -0400

    aplicar descuento segun el precio

--- src/totalizador.js
+++ src/totalizador.js
@@ -21,6 +21,14 @@ function calcularTotalConImpuesto(precioNeto, estado) {
     const impuesto = obtenerImpuesto(estado);
     return precioNeto + (precioNeto * impuesto / 100);
 }
+function obtenerDescuento(total) {
+    if (total >= 30000) return 15;
+    if (total >= 10000) return 10;
+    if (total >= 7000) return 7;
+    if (total >= 3000) return 5;
+    if (total >= 1000) return 3;
+    return 0;
+}
 
 
-module.exports = { ingresarCantidad,ingresarPrecio,calcularPrecioNeto,obtenerImpuesto,calcularTotalConImpuesto };
+module.exports = { ingresarCantidad,ingresarPrecio,calcularPrecioNeto,obtenerImpuesto,calcularTotalConImpuesto,obtenerDescuento };
--- src/totalizador.spec.js
+++ src/totalizador.spec.js
@@ -3,6 +3,7 @@ const { ingresarPrecio } = require('./totalizador');
 const { calcularPrecioNeto } = require('./totalizador');
 const { obtenerImpuesto } = require('./totalizador');
 const { calcularTotalConImpuesto } = require('./totalizador');
+const { obtenerDescuento } = require('./totalizador');
 test('Debe ingresar cantidad de ítems y mostrar el valor', () => {
     expect(ingresarCantidad(20)).toBe(20);
 });
@@ -19,8 +20,14 @@ test('Debe retornar el porcentaje de impuesto según el estado', () => {
     expect(obtenerImpuesto('TX')).toBe(6.25);
     expect(obtenerImpuesto('AL')).toBe(4.00);
 });
-
-
 test('Debe calcular el total con impuestos', () => {
     expect(calcularTotalConImpuesto(60, 'CA')).toBe(64.95); // 60 + (60 * 8.25 / 100)
+});
+test('Debe aplicar el descuento correcto según el precio total', () => {
+    expect(obtenerDescuento(1000)).toBe(3);
+    expect(obtenerDescuento(3000)).toBe(5);
+    expect(obtenerDescuento(7000)).toBe(7);
+    expect(obtenerDescuento(10000)).toBe(10);
+    expect(obtenerDescuento(30000)).toBe(15);
+    expect(obtenerDescuento(500)).toBe(0);
 });
